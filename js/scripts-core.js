52ce6ac2e2c409e2d1d6172c95d2abbdc7df4939
sanographix <mail@sanographix.net>
2015-02-28 10:37:23 +0900

    グローバルヘッダにシェアボタン

--- js/scripts-core.js
+++ js/scripts-core.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
+
+    //ロード or スクロールされると実行
+    $(window).on('load scroll', function(){
+        //ヘッダーの高さ分スクロールするとクラスを追加
+        if ($(window).scrollTop() > 140) {
+            $('.js-globalheader').addClass('is-scrolled');
+        } else {
+            //80px以下だとクラスを削除
+            $('.js-globalheader').removeClass('is-scrolled');
+        }
+    });
+
     // 外部サイトにはtarget blankする
     $('a').each(function() {
       var a = this;
