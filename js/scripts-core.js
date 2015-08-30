d384504428e48be65033d02e36f3ee3c8dee294c
sanographix <mail@sanographix.net>
2015-08-30 14:21:27 +0900

    rin3のgh-pagesを移植

--- js/scripts-core.js
+++ js/scripts-core.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
-    // Add target="_blank" when user opens external link
+
+    //ロード or スクロールされると実行
+    $(window).on('load scroll', function(){
+        //ヘッダーの高さ分スクロールするとクラスを追加
+        if ($(window).scrollTop() > 140) {
+            $('.js-globalheader').addClass('is-scrolled');
+        } else {
+            //80px以下だとクラスを削除
+            $('.js-globalheader').removeClass('is-scrolled');
+        }
+    });
+
+    // 外部サイトにはtarget blankする
     $('a').each(function() {
       var a = this;
       if (a.origin !== location.origin) {
