fa9bd1070342b15f8c81741a8ac6fe929f4a0340
sanographix <mail@sanographix.net>
2016-06-17 20:46:36 +0900

    change index.html directory

--- gulpfile.js
+++ gulpfile.js
@@ -49,7 +49,7 @@ gulp.task("ejs", function() {
     gulp.src(['templates/*.ejs','!' + 'templates/_*.ejs']) // Don't build html which starts from underline
         .pipe(plumber())
         .pipe(ejs(json, {"ext": ".html"}))
-        .pipe(gulp.dest('./'))
+        .pipe(gulp.dest('build'))
 });
 
 // Static server
@@ -57,7 +57,7 @@ gulp.task("ejs", function() {
 gulp.task('browser-sync', function() {
     browserSync({
         server: {
-            baseDir: "./", //　Target directory
+            baseDir: "build/", //　Target directory
             index  : "index.html" // index file
         }
     });
@@ -75,6 +75,6 @@ gulp.task('default',['browser-sync'], function() {
     gulp.watch('sass/**/*.scss',['sass']);
     gulp.watch('js/*.js',['js']);
     gulp.watch('images/**/*.{png,jpg,gif,svg}',['imagemin']);
-    gulp.watch("./*.html", ['bs-reload']);
+    gulp.watch("build/*.html", ['bs-reload']);
     gulp.watch(['templates/*.ejs', 'site.json'], ['ejs']);
 });
