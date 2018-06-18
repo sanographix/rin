ab68eeba484e63c1fb11b706a1215389ba4a65ac
sanographix <mail@sanographix.net>
2018-06-19 00:01:21 +0900

    bs-config

---
+++ bs-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    files: ['build/css/style.css'],
+    server: {
+        baseDir: "build"
+    },
+    open: false,
+    socket: {
+        domain: 'localhost:3000'
+    },
+};
